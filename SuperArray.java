609ec1c1d030d48f78b20aec3d22d5a06b44dcfd
Timothy841 <tsit20@stuy.edu>
2020-11-04 12:10:11 -0500

    Created all the methods. Testing right now.

--- SuperArray.java
+++ SuperArray.java
@@ -8,6 +8,11 @@ public class SuperArray{
     data = new String[10];
   }
 
+  public SuperArray(int initialCapacity){
+    size = 0;
+    data = new String[initialCapacity];
+  }
+
   public int size(){
     return size;
   }
@@ -33,10 +38,72 @@ public class SuperArray{
 
   private void resize(){
     SuperArray sp = new SuperArray();
-    sp.data = new String[data.length + 10];
+    sp.data = new String[data.length*2+1];
     for (int i = 0; i < size; i++){
       sp.data[i] = data[i];
     }
     data = sp.data;
+  }
+
+  public boolean isEmpty(){
+    if (size == 0){
+      return true;
+    }
+    return false;
+  }
+
+  public void clear(){
+    data = new String[data.length];
+    size = 0;
+  }
+
+  public String toString(){
+    String a = "[";
+    for (int i = 0; i<size;i++){
+      if (i<size-1){
+        a+=data[i]+", ";
+      }
+      else{
+        a+=data[i];
+      }
+    }
+    a+="]";
+    return a;
+  }
+
+  public boolean contains(String s){
+    for (int i = 0; i<size && data[i].equals(s);i++){
+      return true;
+    }
+    return false;
+  }
+
+  public void add(int index, String element){
+    if (size==data.length){
+      this.resize();
+    }
+    for (int i = size; i >=index; i--){
+      data[i]=data[i-1];
+    }
+    data[index]=element;
+    size++;
+  }
+
+  public String remove(int index){
+    String a = data[index];
+    for (int i = index; i <size; i++){
+      data[i]=data[i+1];
+    }
+    size--;
+    return a;
+  }
+
+  public int indexOf(String s){
+    for (int i = 0; i<size; i++){
+      if (data[i].equals(s)){
+        return i;
+      }
+    }
+    return -1;
   }
 }

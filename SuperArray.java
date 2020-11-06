5cc7a09abafda786fed4e49b9c62673bf048b8a6
Timothy841 <tsit20@stuy.edu>
2020-11-06 12:16:32 -0500

    Added lastIndexOf. Forgot to make the condition >= 0 instead of == 0. It took so long to see.

--- SuperArray.java
+++ SuperArray.java
@@ -115,6 +115,15 @@ public class SuperArray{
     return -1;
   }
 
+  public int lastIndexOf(String value){
+    for (int i = size-1; i >= 0; i--){
+      if (data[i].equals(value)){
+        return i;
+      }
+    }
+    return -1;
+  }
+
   public String[] toArray(){
     String[] a = new String[size];
     for (int i = 0; i<size;i++){
@@ -122,4 +131,5 @@ public class SuperArray{
     }
     return a;
   }
+
 }

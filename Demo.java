571e2f3875e001e42ff2bb0c1203e8bb2eed675c
Timothy841 <tsit20@stuy.edu>
2020-11-07 20:32:44 -0500

    zip method works too.

--- Demo.java
+++ Demo.java
@@ -23,9 +23,28 @@ public class Demo{
     return c;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b){
+    int c = Math.min(a.size(),b.size());
+    SuperArray d = new SuperArray();
+    for (int i = 0; i< c; i++){
+      d.add(a.toArray()[i]);
+      d.add(b.toArray()[i]);
+    }
+    if (c ==a.size()){
+      for (int i = c; i< b.size(); i++){
+        d.add(b.toArray()[i]);
+      }
+    }
+    else{
+      for (int i = c; i< a.size(); i++){
+        d.add(a.toArray()[i]);
+      }
+    }
+    return d;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
-    //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
@@ -48,8 +67,23 @@ public class Demo{
     b.add("2");
     b.add("2");
     b.add("9");
+    b.add("2");
+    b.add("9");
+    b.add("2");
+    b.add("9");
     System.out.println(a.toString());
     System.out.println(b.toString());
+    System.out.println(zip(a,b).toString());
     System.out.println(findOverlap(a,b));
+    a.clear();
+    b.clear();
+    a.add("9");
+    a.add("1");
+    a.add("2");
+    b.add("9");
+    b.add("1");
+    b.add("2");
+    b.add("");
+    System.out.println(a.equals(b));
   }
 }
